945afef44fe75206db5ac65c2619b5a9ab4ae1ef
okidijimmy200 <okidijimmie@gmail.com>
2023-05-08 15:13:58 +0300

    feature: mixture

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,6 +1,11 @@
+import pytest
 from django.test import TestCase
-from classroom.models import Student
+from classroom.models import Student, ClassRoom
 from mixer.backend.django import mixer
+
+
+pytestmark = pytest.mark.django_db
+
 
 # test the student model
 class TestStudentModel(TestCase):
@@ -11,22 +16,29 @@ class TestStudentModel(TestCase):
 
     # test to check that student can be created
     def test_student_can_be_created(self):
-        mixer.blend(Student, first_name='jimmy')
+        mixer.blend(Student, first_name="jimmy")
         student_result = Student.objects.last()
-        self.assertEqual(student_result.first_name, "jimmy")
+        assert student_result.first_name == "jimmy"
 
     def test_str_return(self):
         # mixer to auto-generate model values
-        mixer.blend(Student, first_name='jimmy')
+        mixer.blend(Student, first_name="jimmy")
         student_result = Student.objects.last()
-        self.assertEqual(str(student_result), "jimmy")
+        assert str(student_result) == "jimmy"
 
     def test_grade_fail(self):
         mixer.blend(Student, average_score=10)
         student_result = Student.objects.last()
-        self.assertEqual(student_result.get_grade(), "Fail")
+        assert student_result.get_grade() == "Fail"
 
     def test_grade_pass(self):
         mixer.blend(Student, average_score=80)
         student_result = Student.objects.last()
-        self.assertEqual(student_result.get_grade(), "Excellent")
+        assert student_result.get_grade(), "Excellent"
+
+
+class TestClassroomModel:
+    def test_classrom_create(self):
+        mixer.blend(ClassRoom, name="physics")
+        classroom_result = ClassRoom.objects.last()
+        assert str(classroom_result) == "physics"
